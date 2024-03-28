c3093a792a2c27df1265c9ccf28f52aa5a5ec58d
Ankur1493 <ankursharma1493@gmail.com>
2024-03-28 21:09:38 +0530

    implemented google oauth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import express from "express";
-
+import { google } from "googleapis"
+import dotenv from "dotenv";
+dotenv.config()
 const app = express();
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
+);
+
+const scopes = [
+  'https://www.googleapis.com/auth/calendar'
+];
+
+app.get("/google", (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes
+  });
+  res.redirect(url);
+})
+
+app.get("/", (req, res) => {
+  res.send("user authenticated, it's working")
+})
 
 app.listen(5000, () => {
   console.log("server running on port 5000")
