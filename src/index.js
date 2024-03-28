bc092fb8ca9a2c2eb27fb3cf5f34cc94276d3c8a
Ankur1493 <ankursharma1493@gmail.com>
2024-03-28 22:03:24 +0530

    implemened

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { google } from "googleapis"
 import dotenv from "dotenv";
+import { info } from "console";
 dotenv.config()
 const app = express();
 
@@ -9,6 +10,34 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_SECRET,
   process.env.REDIRECT_URL
 );
+
+const calendar = google.calendar({
+  version: "v3",
+  auth: process.env.API_KEY,
+})
+const event = {
+  'summary': 'test',
+  'location': 'A93 st6 east Azad Nagar',
+  'description': 'This is the most important test',
+  'start': {
+    'dateTime': '2024-03-29T15:00:00+05:30',
+    'timeZone': 'Asia/Kolkata',
+  },
+  'end': {
+    'dateTime': '2024-03-29T17:00:00+05:30',
+    'timeZone': 'Asia/Kolkata',
+  },
+  'attendees': [
+    { 'email': 'uditkapoor060@gmail.com' },
+  ],
+  'reminders': {
+    'useDefault': false,
+    'overrides': [
+      { 'method': 'email', 'minutes': 24 * 60 },
+      { 'method': 'popup', 'minutes': 10 },
+    ],
+  },
+};
 
 const scopes = [
   'https://www.googleapis.com/auth/calendar'
@@ -26,10 +55,25 @@ app.get("/", async (req, res) => {
   const { code } = req.query;
   const { tokens } = await oauth2Client.getToken(code)
   oauth2Client.setCredentials(tokens);
-  console.log(code);
 
   res.send("user authenticated, it's working")
 })
+
+
+app.get("/schedule_event", async (req, res) => {
+
+  console.log(oauth2Client.credentials.access_token)
+
+  await calendar.events.insert({
+    calendarId: "primary",
+    auth: oauth2Client,
+    requestBody: event,
+    sendUpdates: "all"
+  });
+
+  res.send("Completed");
+});
+
 
 app.listen(5000, () => {
   console.log("server running on port 5000")
